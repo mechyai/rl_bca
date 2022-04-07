e2250045ddc2c610a5f0d0f698d887d0c7cbfe56
Chris (mechyai) <jpeubel.ai@gmail.com>
2022-04-07 15:43:06 -0400

    included PER for BDQ

--- bca/__init__.py
+++ bca/__init__.py
@@ -1,4 +1,4 @@
-from .bdq import BranchingDQN, EpsilonGreedyStrategy, ReplayMemory
+from .bdq import BranchingDQN, EpsilonGreedyStrategy, ReplayMemory, PrioritizedReplayMemory
 from .bdq_rnn import BranchingDQN_RNN, SequenceReplayMemory
 from .tensorboard_manager import TensorboardManager
 from .agent_tb import Agent as Agent_TB
