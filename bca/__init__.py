867d517fda73e8d8c6c733d23cf86fe5684aecf2
Chris (mechyai) <jpeubel.ai@gmail.com>
2022-04-21 21:02:26 -0400

    dqn added

--- bca/__init__.py
+++ bca/__init__.py
@@ -2,5 +2,6 @@ from .MDP import *
 
 from .bdq import BranchingDQN, EpsilonGreedyStrategy, ReplayMemory, PrioritizedReplayMemory
 from .bdq_rnn import BranchingDQN_RNN, SequenceReplayMemory, PrioritizedSequenceReplayMemory
+from .dqn import DQN
 
 from .agent import Agent
