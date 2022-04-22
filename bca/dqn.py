e01c563cb746b86f0d82a28837a32be8ddf71383
Christopher Eubel <ceubel@path-robotics.com>
2022-04-22 09:13:04 -0400

    got rid of putting losses to cpu

--- bca/dqn.py
+++ bca/dqn.py
@@ -160,7 +160,7 @@ class DQN(nn.Module):
 
         self.step_count += 1
 
-        return float(loss_total.detach().cpu()), loss_each.detach().mean(dim=1).cpu()
+        return float(loss_total.detach().cpu()), loss_each.detach().mean(dim=1)
 
     def import_model(self, model_path: str):
         """
