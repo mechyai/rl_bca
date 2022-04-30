b54baf77e0841d3d4a7d7453926516dc5abb1c79
Chris (mechyai) <jpeubel.ai@gmail.com>
2022-04-29 23:56:10 -0400

    fixed temp

--- bca_manager/tensorboard_manager.py
+++ bca_manager/tensorboard_manager.py
@@ -50,7 +50,7 @@ class TensorboardManager:
         #     cooling_sp = 29.4
         #     heating_sp = 15.6
         # self.tb.add_scalar('Thermostat/Cooling_SP', cooling_sp, agent.current_step)
-        self.tb.add_scalar('Thermostat/Zone1_Temp', agent.sim.get_ems_data(['zn1_temp']), agent.current_step)
+        self.tb.add_scalar('Thermostat/Zone1_Temp', agent.sim.get_ems_data(['zn0_temp']), agent.current_step)
         # self.tb.add_scalar('Thermostat/Heating_SP', heating_sp, agent.current_step)
 
         # -- Sim Results --
