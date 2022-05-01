6eee07591de53b4ad1b0ac552a7ff1cbf2c80dda
Chris (mechyai) <jpeubel.ai@gmail.com>
2022-05-01 14:35:17 -0400

    fixed reporting and saving sql

--- Current_Prototype/hparam_scheduler_runner.py
+++ Current_Prototype/hparam_scheduler_runner.py
@@ -15,21 +15,23 @@ from bca_manager import _paths_config, experiment_manager
 year = MDP.year
 train_month_start = 'April'
 train_month_end = 'April'
+train_day_start = 21
 train_day_start = 30
 train_day_end = None
 
 test_month_start = 'May'
 test_month_end = 'May'
-test_day_start = None
-test_day_end = 30
+test_day_start = 14
+test_day_start = 30
+test_day_end = None
 
-model_name = 'BEM_5z_2A_Base_Testbed_no_ventilation_oa1_STRAIGHT.osm'
+# model_name = 'BEM_5z_2A_Base_Testbed_no_ventilation_oa1_STRAIGHT.osm'
 model_name = 'BEM_5z_2A_Base_Test.osm'
-
-run_modification = [5e-3, 1e-3, 5e-4, 5e-5, 1e-5, 5e-6, 1e-6]
 
 # exp_name = 'act5_duel_DQN_overfit_rtp_weight_hparam'
 exp_name = 'test'
+
+reporting_freq = 1
 
 # -- Experiment Params --
 experiment_params_dict = {
@@ -38,7 +40,7 @@ experiment_params_dict = {
     'run_index_limit': 100,
     'skip_baseline': False,
     'experiment_desc': '',
-    'print_values': True
+    'print_values': False
 }
 
 # -------------------------------------------------- START PIPELINE --------------------------------------------------
@@ -157,7 +159,6 @@ for run_num, run in enumerate(runs):
     my_bdq = run_manager.create_bdq(run)
     my_memory = run_manager.create_replay_memory(run)
 
-    reporting_freq = 10
     start_step = 0
     continued_params_dict = {'epsilon_start': run.eps_start}
     if run.PER:
@@ -166,13 +167,13 @@ for run_num, run in enumerate(runs):
 
     for epoch in range(experiment_params_dict['epochs']):
 
-        print(f'\nRun {run_num + 1} of {run_limit}, Epoch {epoch + 1} of {experiment_params_dict["epochs"]}\n{run}\n')
+        print(f'\nRun {run_num} of {run_limit}, Epoch {epoch + 1} of {experiment_params_dict["epochs"]}\n{run}\n')
 
         # ---- Tensor Board ----
         my_tb = TensorboardManager(
             run_manager,
             name_path=os.path.join(exp_folder,
-                                   f'run_{run_num + 1}-{run_limit}_TRAIN_'
+                                   f'run_{run_num}-{run_limit}_TRAIN_'
                                    f'{experiment_params_dict["epochs"]}_{train_period}')
         )
 
@@ -224,7 +225,7 @@ for run_num, run in enumerate(runs):
         # -- Save Model Intermediate --
         if ((epoch + 1) % reporting_freq == 0 or epoch == experiment_params_dict['epochs'] - 1) and epoch != 0:
             print('\n********** Saved Model ************\n')
-            model_name = f'bdq_epoch_{epoch}_of_{experiment_params_dict["epochs"]}'
+            model_name = f'_bdq_model_run_{run_num}_epoch_{epoch}_of_{experiment_params_dict["epochs"]}'
             torch.save(my_bdq.policy_network.state_dict(),
                        os.path.join(exp_folder, model_name))
 
@@ -234,7 +235,7 @@ for run_num, run in enumerate(runs):
     my_tb = TensorboardManager(
         run_manager,
         name_path=os.path.join(exp_folder,
-                               f'run_{run_num + 1}-{run_limit}_EXPLOIT_epoch{epoch + 1}-'
+                               f'run_{run_num}-{run_limit}_EXPLOIT_epoch{epoch + 1}-'
                                f'{experiment_params_dict["epochs"]}_{train_period}')
     )
 
@@ -266,12 +267,18 @@ for run_num, run in enumerate(runs):
         run_type=run_type
     )
 
+    # Save SQL
+    shutil.copy(os.path.join(_paths_config.repo_root, r'Current_Prototype/out/eplusout.sql'), exp_folder)
+    time.sleep(1)
+    os.rename(os.path.join(exp_folder, 'eplusout.sql'),
+              os.path.join(exp_folder, f'_{train_period}_run_{run_num}_ep{epoch + 1}_EXPLOIT_SQL.sql'))
+
     print('\n********** Test **********\n')
 
     my_tb = TensorboardManager(
         run_manager,
         name_path=os.path.join(exp_folder,
-                               f'run_{run_num + 1}-{run_limit}_TEST_epoch{epoch + 1}-'
+                               f'run_{run_num}-{run_limit}_TEST_epoch{epoch + 1}-'
                                f'{experiment_params_dict["epochs"]}_{test_period}')
     )
 
@@ -300,6 +307,12 @@ for run_num, run in enumerate(runs):
         epoch=0,
         run_type=run_type
     )
+
+    # Save SQL
+    shutil.copy(os.path.join(_paths_config.repo_root, r'Current_Prototype/out/eplusout.sql'), exp_folder)
+    time.sleep(1)
+    os.rename(os.path.join(exp_folder, 'eplusout.sql'),
+              os.path.join(exp_folder, f'_{test_period}_run_{run_num}_ep{epoch + 1}_TEST_SQL.sql'))
 
     exp_file = os.path.join(exp_folder, '_exp_results_log.txt')
     # -- Save / Write Data --
