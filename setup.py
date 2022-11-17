49476b784d95012c256f4276455da82381a7857e
Yashwanth <yashwanth.alakky@gmail.com>
2022-11-17 13:14:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='rl_bca',
     version='0.0.1',
-    packages=['bca'],
+    packages=['bca', 'bca_manager'],
     url='',
     license='Apache License 2.0',
     author='Chris Eubel',
